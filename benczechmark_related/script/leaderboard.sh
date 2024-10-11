d721ca0606ac7d204df06645c69010cae483d4e2
mlynatom <mlynatom@fel.cvut.cz>
2024-10-11 15:59:59 +0200

    command-r benchmark results

--- benczechmark_related/script/leaderboard.sh
+++ benczechmark_related/script/leaderboard.sh
@@ -4,17 +4,17 @@
 #SBATCH --ntasks-per-node 1
 #SBATCH --time 12:00:00
 #SBATCH --job-name benczechmark_eval_cpu
-#SBATCH --output logs/benczechmark-logs.%j.out
+#SBATCH --output logs/benczechmark-logs-leaderboard.%j.out
 
 ml Python/3.10.4-GCCcore-11.3.0-bare
 
 # Replace with your own virtual environment
 source ~/venvs/benczechmark/bin/activate
 
-cd benczechmark-leaderboard
+cd ../benczechmark-leaderboard
 
 echo $(pwd)
 
 export PYTHONPATH=$(pwd)
 
-python /home/mlynatom/benczechmark-leaderboard/leaderboard/compile_log_files.py -i "/home/mlynatom/master-thesis-repository-tomas-mlynar/results/eval_llama32-3B_instruct*" -o /home/mlynatom/master-thesis-repository-tomas-mlynar/llama3.2-3B-Instruct_submission.json
+python /home/mlynatom/benczechmark-leaderboard/leaderboard/compile_log_files.py -i "/home/mlynatom/master-thesis-repository-tomas-mlynar/results/eval_c4ai-command-r-08-2024*" -o /home/mlynatom/master-thesis-repository-tomas-mlynar/eval_c4ai-command-r-08-2024_submission.json
--- benczechmark_related/script/test_benczechmark_command-r.sh
+++ benczechmark_related/script/test_benczechmark_command-r.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
 #SBATCH --mem-per-cpu 96G
-#SBATCH --gres gpu:2
-#SBATCH --time 1:00:00
+#SBATCH --gres gpu:4
+#SBATCH --time 3:00:00
 #SBATCH --job-name benczechmark_eval
 #SBATCH --output logs/test_benczechmark_command-r.%j.out
 
@@ -18,10 +18,10 @@ export PYTHONPATH=src:$PYTHONPATH
 
 python -m lm_eval \
     --model vllm \
-    --model_args pretrained=AMead10/c4ai-command-r-08-2024-awq,tensor_parallel_size=2,dtype=float16,gpu_memory_utilization=0.5,max_length=2048,normalize_log_probs=True,trust_remote_code=False,truncate_strategy=leave_description,quantization=awq \
-    --tasks benczechmark_ctkfacts_nli \
+    --model_args pretrained=AMead10/c4ai-command-r-08-2024-awq,tensor_parallel_size=2,dtype=float16,gpu_memory_utilization=0.4,max_length=2048,normalize_log_probs=True,trust_remote_code=False,truncate_strategy=leave_description,quantization=awq \
+    --tasks benczechmark_grammarerrorcorrection \
     --batch_size auto:4 \
-    --output_path results_test_benczechmark/eval_command-r \
+    --output_path results/eval_c4ai-command-r-08-2024-awq_benczechmark_grammarerrorcorrection \
     --log_samples \
     --verbosity DEBUG \
     --num_fewshot 3
