b58e932a3f2c61dac52f1e30bc4c38656c068e2c
mlynatom <mlynatom@fel.cvut.cz>
2025-01-06 21:44:13 +0100

    instruction tuning script

---
+++ scripts/models/it_exp_amdgpu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=8:00:00
+#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=4
+#SBATCH --partition=amdgpu --gres=gpu:1
+#SBATCH --mem-per-cpu 54G
+#SBATCH --job-name gpu_experiments
+#SBATCH --output /home/mlynatom/master-thesis-repository-tomas-mlynar/logs/models/it_exp_amdgpu.%J.log
+
+
+ml Python/3.12.3-GCCcore-13.3.0 
+
+source /home/mlynatom/master-thesis-repository-tomas-mlynar/venv/master_venv/bin/activate
+
+python /home/mlynatom/master-thesis-repository-tomas-mlynar/training/instruction_tuning.py
